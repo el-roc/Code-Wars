cb0d48e86867c9dc23eef19141882d6568a51bf7
el-roc <eric.eespinoza@gmail.com>
2020-03-17 19:30:12 -0400

    push 5 without numbers

---
+++ 5 without numbers.js
@@ -0,0 +1,13 @@
+// Write a function that always returns 5
+//
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+//
+// Good luck :)
+//p function, array or string that has a length of 5
+//R get value 5 without using numbers or characters 0123456789*+-/
+//E ["","","","",""]
+//Pseudo make an array that returns the value five and get the length to return 5
+function unusualFive() {
+  let array = ["a", "b", "c", "d", "e"]
+  return array.length
+}
